8f985b3eaf5b7d1aae0cc6b72a3b8ff7622c940a
robecor <robert.corolea@cultofcoders.com>
2020-01-15 23:59:50 +0200

    Certificate generation

--- cert_service/index.js
+++ cert_service/index.js
@@ -1,8 +1,24 @@
 const kue = require('kue');
 const queue = kue.createQueue();
 const jobEnums = require('../enums/jobEnums');
+const selfsigned = require('selfsigned');
 
 queue.process(jobEnums.CREATE_CERT, function (job, done) {
-  console.log(job.data);
-  done();
+  const {domain, method} = job.data;
+
+  const certAttributes = [
+    {
+      name: 'commonName',
+      value: domain
+    },
+  ];
+
+  selfsigned.generate(certAttributes, {days: 365}, function (err, pems) {
+    if (err) {
+      console.log(err);
+      return done(new Error(err));
+    }
+
+    done(pems);
+  });
 });
---
+++ enums/methodEnum.js
@@ -0,0 +1,5 @@
+module.exports = [
+  'GET',
+  'POST',
+  'PUT'
+];
